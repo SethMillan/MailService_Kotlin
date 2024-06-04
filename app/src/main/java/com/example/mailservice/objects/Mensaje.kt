1d59611cd44ee998d00cdd6e8a35db2c4000bee9
SethMillan <millanseth@gmail.com>
2024-06-04 04:00:03 -0600

    update: data class Mensaje terminada

--- app/src/main/java/com/example/mailservice/objects/Mensaje.kt
+++ app/src/main/java/com/example/mailservice/objects/Mensaje.kt
@@ -1,3 +1,14 @@
 package com.example.mailservice.objects
 
-data class Mensaje()
+import java.io.Serializable
+
+data class Mensaje(
+    val id: Int,
+    val idConversacion: Int,
+    val correoEmisor: String,
+    val correoReceptor: String,
+    val asunto: String,
+    val mensaje: String,
+    val recibido: Boolean,
+    val leido: Boolean
+): Serializable
